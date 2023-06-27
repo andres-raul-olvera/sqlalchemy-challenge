e8304089312a9ddc2e38a43c0ab5ade691ffb5de
andres-raul-olvera <andresrolvera@gmail.com>
2023-06-26 22:26:27 -0500

    turning in late

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+
+
+
+#################################################
+# Flask Setup
+#################################################cd
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt/&lt;end6&gt"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
